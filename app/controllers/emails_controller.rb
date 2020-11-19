460d1e1d898c0016a3003222a94785d77ef9c9c8
bkany <bkany@student.42.fr>
2020-11-19 11:26:53 +0100

    adding the form-post-button for adding a new email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,30 @@
 class EmailsController < ApplicationController
+
+  def new
+    @emails = Email.all
+  end
+
+  def create
+    @email = Email.new()
+    @email.object = Faker::Color.color_name
+    @email.body = Faker::GreekPhilosophers.quote
+    
+    if @email.save
+      flash[:notice] = "Email added"
+      respond_to do |format|
+      	format.html { redirect_to emails_path}
+      	format.js { }
+      end
+      
+    else
+    	flash[:notice] = "Please try again"
+      respond_to do |format|
+      	format.html { redirect_to emails_path}
+      	format.js { }
+      end
+    end
+    
+  end
+
+
 end
